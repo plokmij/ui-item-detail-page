a6c0b5ee11e51eea33a5e5a73a72c90317c95bfa
plokmij <samfan@live.com>
2019-03-01 04:40:25 +0530

    refactor

--- lib/src/screens/detail_page.dart
+++ lib/src/screens/detail_page.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import '../widgets/appbar_with_pics.dart';
 
-class ItemDetail extends StatelessWidget{
-  Widget build(BuildContext context){
+class ItemDetail extends StatelessWidget {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: SilAppBar(),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              leading: Icon(Icons.arrow_back),
+              expandedHeight:
+                  (MediaQuery.of(context).size.height / 2) * (3 / 4),
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                background: imageContainer(),
+              ),
+            ),
+          ];
+        },
+        body: Center(
+          child: Text("Body und ketto"),
+        ),
+      ),
+    );
+  }
+
+  Widget imageContainer() {
+    return Container(
+      height: 200.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: Colors.blue,
+      ),
     );
   }
 }
