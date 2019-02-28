1729356e7f06f36e37858d07a156d8cbcfa80869
plokmij <samfan@live.com>
2019-03-01 04:37:29 +0530

    silver appbar

---
+++ lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'screens/detail_page.dart';
+
+class App extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home:ItemDetail(),
+    );
+  }
+}
---
+++ lib/src/screens/detail_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import '../widgets/appbar_with_pics.dart';
+
+class ItemDetail extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SilAppBar(),
+    );
+  }
+}
