4daa404929ce16138c22d55e6dde08324b5ede71
plokmij <samfan@live.com>
2019-03-01 06:51:39 +0530

    features row

--- lib/src/widgets/titlecard.dart
+++ lib/src/widgets/titlecard.dart
@@ -16,10 +16,12 @@ class TitleCard extends StatelessWidget {
     fontSize: 14.0,
   );
 
+  Color iconColor = Colors.grey;
+
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      padding: EdgeInsets.symmetric(horizontal: 10.0,vertical: 5.0),
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
       child: Column(
         children: <Widget>[
           buildNameRow(),
@@ -27,43 +29,121 @@ class TitleCard extends StatelessWidget {
             color: Colors.grey,
             height: 20.0,
           ),
+          buildFeaturesRow(),
         ],
       ),
     );
   }
 
-  Widget buildNameRow(){
-   return Row(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Center(child: Text("Logo")),
-            margin: EdgeInsets.only(right: 10.0),
-            decoration: BoxDecoration(
-              color: Colors.red,
+  Widget buildNameRow() {
+    return Row(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(20.0),
+          child: Center(child: Text("Logo")),
+          margin: EdgeInsets.only(right: 10.0),
+          decoration: BoxDecoration(
+            color: Colors.red,
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              name,
+              textAlign: TextAlign.left,
+              style: header,
+            ),
+            Text(
+              address,
+              textAlign: TextAlign.left,
+              style: subtitle,
+            ),
+          ],
+        ),
+        Spacer(),
+        Icon(
+          Icons.location_on,
+          size: 48.0,
+        ),
+      ],
+    );
+  }
+
+  Widget buildFeaturesRow() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.add_a_photo,
+                  size: 32.0,
+                  color: iconColor,
+                ),
+                Text("Feature")
+              ],
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                name,
-                textAlign: TextAlign.left,
-                style: header,
-              ),
-              Text(
-                address,
-                textAlign: TextAlign.left,
-                style: subtitle,
-              ),
-            ],
+        ),
+        Expanded(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.add_a_photo,
+                  size: 32.0,
+                  color: iconColor,
+                ),
+                Text("Feature")
+              ],
+            ),
           ),
-          Spacer(),
-          Icon(
-            Icons.location_on,
-            size: 48.0,
+        ),
+        Expanded(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.add_a_photo,
+                  size: 32.0,
+                  color: iconColor,
+                ),
+                Text("Feature")
+              ],
+            ),
           ),
-        ],
-      );
+        ),
+        Expanded(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.add_a_photo,
+                  size: 32.0,
+                  color: iconColor,
+                ),
+                Text("Feature")
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.add_a_photo,
+                  size: 32.0,
+                  color: iconColor,
+                ),
+                Text("Feature")
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
