b3011b12b358a03171b64ef5860b2f012980b47a
plokmij <samfan@live.com>
2019-03-03 00:42:06 +0530

    size streambuild

--- lib/src/blocs/date_time_bloc.dart
+++ lib/src/blocs/date_time_bloc.dart
@@ -5,21 +5,26 @@ class DateTimeBloc {
   final _startTime = BehaviorSubject();
   final _endTime = BehaviorSubject();
   final _date = BehaviorSubject();
+  final _size = BehaviorSubject();
 
   Stream get startTime => _startTime.stream;
   Stream get endTime => _endTime.stream;
   Stream get date => _date.stream;
+  Stream get size => _size.stream;
 
   Function get changeStartTime => _startTime.sink.add;
   Function get changeEndTime => _endTime.sink.add;
-  Function get changeDate => _date.sink.add; 
+  Function get changeDate => _date.sink.add;
+  Function get changSize => _size.sink.add;
 
   getTime() {
     print(_startTime.value);
     print(_endTime.value);
+    print(_size.value);
   }
 
   dispose() {
+    _size.close();
     _startTime.close();
     _endTime.close();
     _date.close();
--- lib/src/widgets/size_selector.dart
+++ lib/src/widgets/size_selector.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import '../blocs/date_time_provider.dart';
 
 class SizeSelector extends StatelessWidget {
   Widget build(BuildContext context) {
+    final dateTimeBloc = DateTimeProvider.of(context);
     return Container(
       margin: EdgeInsets.only(bottom: 5.0),
       padding: EdgeInsets.symmetric(vertical: 20.0),
@@ -9,11 +11,12 @@ class SizeSelector extends StatelessWidget {
         shape: BoxShape.rectangle,
         color: Colors.white,
       ),
-      child: buildRow(),
+      child: buildRow(dateTimeBloc),
     );
   }
 
-  Widget buildRow() {
+  Widget buildRow(DateTimeBloc dateTimeBloc) {
+    dateTimeBloc.changSize("30");
     return Column(
       children: <Widget>[
         Container(
@@ -25,54 +28,82 @@ class SizeSelector extends StatelessWidget {
             Expanded(
               child: Column(
                 children: <Widget>[
-                  Container(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black54,
-                          offset: Offset(1.0, 2.0),
-                        )
-                      ],
-                      shape: BoxShape.rectangle,
-                      color: Color(0xffff4c5d),
-                    ),
-                    child: Text(
-                      "30\"",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0),
-                    ),
-                  )
+                  StreamBuilder(
+                    stream: dateTimeBloc.size,
+                    builder: (context, snapshot) {
+                      return GestureDetector(
+                        onTap: () {
+                          dateTimeBloc.changSize("30");
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 20.0,
+                            vertical: 8.0,
+                          ),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                              )
+                            ],
+                            shape: BoxShape.rectangle,
+                            color: snapshot.data == "30"
+                                ? Color(0xffff4c5d)
+                                : Color(0xffcad8e4),
+                          ),
+                          child: Text(
+                            "30\"",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ],
               ),
             ),
             Expanded(
               child: Column(
                 children: <Widget>[
-                  Container(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black54,
-                          offset: Offset(1.0, 2.0),
-                        )
-                      ],
-                      shape: BoxShape.rectangle,
-                      color: Color(0xffcad8e4),
-                    ),
-                    child: Text(
-                      "32\"",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0),
-                    ),
-                  )
+                  StreamBuilder(
+                    stream: dateTimeBloc.size,
+                    builder: (context, snapshot) {
+                      return GestureDetector(
+                        onTap: () {
+                          dateTimeBloc.changSize("32");
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 20.0,
+                            vertical: 8.0,
+                          ),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                              )
+                            ],
+                            shape: BoxShape.rectangle,
+                            color: snapshot.data == "32"
+                                ? Color(0xffff4c5d)
+                                : Color(0xffcad8e4),
+                          ),
+                          child: Text(
+                            "30\"",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ],
               ),
             ),
